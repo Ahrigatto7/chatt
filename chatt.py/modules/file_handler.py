db9f2ff4409e77b12d9ce0840b6aeb89e762469d
kyky <jeje6401@naver.com>
2025-06-30 03:04:07 +0900

    Delete chatt.py directory

--- chatt.py/modules/file_handler.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-
-def extract_text(file):
-    suffix = file.name.split(".")[-1]
-    if suffix == "docx":
-        doc = Document(file)
-        return "\n".join(p.text for p in doc.paragraphs)
-    else:
-        return file.read().decode("utf-8")
